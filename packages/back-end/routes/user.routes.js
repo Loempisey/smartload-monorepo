536cf6fcfcec68b3b4d415940aa8d0ae43ad7728
Chhenglee <77914400+Chhenglee@users.noreply.github.com>
2022-05-07 16:00:58 +0700

    user.routes last update

--- packages/back-end/routes/user.routes.js
+++ packages/back-end/routes/user.routes.js
@@ -1,17 +1,18 @@
 //export default
 const controllers= require('../controllers/user.controller')
 const authJwt = require('../utils/Auth/authJwt')
-module.exports=(test)=>{
-    // app.use((req, res, next) => {
-    //     res.header(
-    //       "Access-Control-Allow-Headers",
-    //       "x-access-token, Origin, Content-Type, Accept"
-    //     );
-    //     next(); 
-    //   });
-    test.get('/users',[authJwt.verifyToken,authJwt.isAdmin],controllers.getUser);
-    test.post('/users',controllers.createUser);
-    test.post('/users/signin',controllers.signin);
-    test.put('/users/:id',controllers.updateUser);
-    test.delete('/users/:id',controllers.deleteUser);
+module.exports=(app)=>{
+    app.use((req, res, next) => {
+        res.header(
+          "Access-Control-Allow-Headers",
+          "x-access-token, Origin, Content-Type, Accept"
+        );
+        next(); 
+      });
+    app.get('/user',[authJwt.verifyToken,authJwt.isAdmin],controllers.getUser);
+    app.post('/user/signup',controllers.createUser);
+    app.post('/user/signin',controllers.signin);
+    app.put('/user/:id',controllers.updateUser);
+    app.delete('/user/:id',controllers.deleteUser);
+    app.get('/user/current_user',[authJwt.verifyToken],controllers.getCurrentUser)
 }
