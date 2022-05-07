aec96424b1a47056e5846d01fee33b3a29743c6f
Chhenglee <77914400+Chhenglee@users.noreply.github.com>
2022-05-07 15:53:36 +0700

    Server last update

--- packages/back-end/server.js
+++ packages/back-end/server.js
@@ -1,58 +1,59 @@
 const express = require('express');
-const test = express();
+const cors = require('cors')
+const app = express();
 const bodyParser = require('body-parser');
 const morgan=require('morgan');
 const db = require('./models')
 const dotenv = require('dotenv');
 
-// app.use(cors({
-//     origin: '*',
-//     methods: ['GET',"POST","PUT","DELETE"],
-//   }))
+app.use(cors({
+    origin: '*',
+    methods: ['GET',"POST","PUT","DELETE"],
+  }))
 // connectionDB();
 
-db.mongoose.connect('mongodb://localhost:27017/test')
+db.mongoose.connect('mongodb://localhost:27017/app')
 dotenv.config({path:'config.env'})
 
 const PORT = process.env.PORT;
-console.log(PORT)
-console.log(process.env.MONGO_URI)
+// console.log(PORT)
+// console.log(process.env.MONGO_URI)
 
 //Log Request
-test.use(morgan('tiny'))
+app.use(morgan('tiny'))
 
 //get data from body
-test.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded())
 //accept json format
-test.use(express.json())
+app.use(express.json())
 
 // Static Route
-require('./routes/AccessRoute.routes')(test)
+require('./routes/AccessRoute.routes')(app)
 
 // Query
 
-require('./routes/QueryStudents.routes')(test)
+require('./routes/QueryStudents.routes')(app)
 
-// test.get("*",(req,res)=>{
+// app.get("*",(req,res)=>{
 //     res.send({message:"Not Found Route."})
 // })
 
-test.post("/class",(req,res)=>{
+app.post("/class",(req,res)=>{
     const body = req.body
     res.status(200).send({message:body})
 })
 
 //Dynamic Route (Params,query)
 
-require('./routes/GetUserInfo')(test)
+require('./routes/GetUserInfo')(app)
 
 
-require('./routes/user.routes')(test)
-require('./routes/class.routes')(test)
-require('./routes/customer.routes')(test)
-require('./routes/order.routes')(test)
-require('./routes/history.routes')(test)
+require('./routes/user.routes')(app)
+require('./routes/class.routes')(app)
+require('./routes/customer.routes')(app)
+require('./routes/order.routes')(app)
+require('./routes/history.routes')(app)
 
-test.listen(PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`Server is starting http://localhost:${PORT}/`)
 })
