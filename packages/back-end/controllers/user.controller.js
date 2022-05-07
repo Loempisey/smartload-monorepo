10a78e70d7a113deccbb944ebd7500b1d12c5b61
Chhenglee <77914400+Chhenglee@users.noreply.github.com>
2022-05-07 16:00:12 +0700

    user.controller last update

--- packages/back-end/controllers/user.controller.js
+++ packages/back-end/controllers/user.controller.js
@@ -2,11 +2,21 @@ const db = require('./../models')
 const isEmail = require('validator/lib/isEmail')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+
+const getCurrentUser= async(req,res)=>{
+    const userId = req.userId;
+    if(!userId) response.errorResponse(res,401,undefined,"Unauthorized");
+    const user = await db.users.findById(userId)
+    if(!user) response.errorResponse(res,401,undefined,"No current user.");
+    res.status (200).send(user);
+
+}
+
 const createUser= async(req,res)=>{
     const{
-        fullname,username,email,password,dob,role
+        fullname,username,email,role,password,
     }=req.body;
-    console.log(fullname,username,email,password,dob,role)
+    console.log(fullname,username,email,role,password,)
     //body empty
     if(Object.keys(req.body).length==0){
         return res.status(400).send({message:'can not empty data.'})
@@ -23,19 +33,20 @@ const createUser= async(req,res)=>{
     try{
         const user = await db.users.findOne({email:email})
         if(user){
-          return res.status(401).send({message:'this eamil is already use.'})
+          return res.status(401).send({statusCode:401,message:'this eamil is already use.'})
         }
         const newUser = new db.users({
             fullname:fullname,
             username:username,
             email:email,
+            role:role,
             password:bcrypt.hashSync(password,8),
-            dob:dob,
-            role:role
+            
+            
         })
     //create a user
         const data = await newUser.save();
-        return res.status(200).send({data})
+        return res.status(200).send({statusCode:200,data})
     }catch(error){
         res.status(500).send({message:error || 'Error is occured.'})
     }
@@ -45,18 +56,19 @@ const signin=async(req,res)=>{
     try{
         const user = await db.users.findOne({email:email})
         if(!user){
-          return res.status(401).send({message:'No user in the system. Please create one'})
+          return res.status(401).send({statusCode:401,message:'No user in the system. Please create one'})
         }
         const isRightPassword = bcrypt.compareSync(password,user.password)
         if(!isRightPassword){
-            return res.status(401).send({message:'Password is not Matched.'})
+            return res.status(401).send({statusCode:401,message:'Password is not Matched.'})
         }
         const playload ={userId:user._id}
+        const userInfo = user._doc
         jwt.sign(playload,process.env.JWT_SECRET_KEY,{expiresIn:'1d'},(err,token)=>{
             if(err){
                return res.status(401).send({error:err})
             }
-            res.status(200).send({data:user,token:token})
+            res.status(200).send({statusCode:200, data:{ ...userInfo,token}})
         })    
     }catch (error){
         res.status(500).send({message:error||'Error is occured.'})
@@ -99,7 +111,8 @@ module.exports={
     signin,
     getUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    getCurrentUser
 }
 
 
